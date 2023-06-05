9d66ec624bed4e7c2ba07905098a936a745ef0a1
UppalapatiSri <93474927+UppalapatiSri@users.noreply.github.com>
2023-06-05 21:51:24 +0530

    trying to make changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var data_1 = {image : "IMG-5640.jpg", name : "Uppalapati Sri", designation : "Software Engineering Student"};
-var data_2 = {image : "IMG-5672.jpg", name : "Sri", designation : "Software Developer"};
+var data_1 = {image : "1.jpg", name : "Uppalapati Sri", designation : "Software Engineering Student"};
+var data_2 = {image : "2.jpg", name : "Sri", designation : "Software Developer"};
 var isdata_1 = true;
 var display_data ;
 var changemypic = function(){
      if (isdata_1){
         display_data = data_2;
-        data_1 = false;
+        isdata_1 = false;
      }
      else{
         display_data = data_1;
-        data_2 = false;
+        isdata_2 = true;
      }
      document.getElementById("myimage").src = display_data.image;
      document.getElementById("myname").innerHTML = display_data.name;
      document.getElementById("mydesig").innerHTML = display_data.designation;
-     
+
 }
