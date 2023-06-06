a527d3f60bee4f9ecdc92eb0348daa4820b97093
UppalapatiSri <93474927+UppalapatiSri@users.noreply.github.com>
2023-06-06 22:00:44 +0530

    trail 3

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ var userinfo = {name : "", image : "", username : ""};
 var changemypic = function(){
     fetch("https://randomuser.me/api").then((respone)=>response.json()).then((data)=>{ 
         userinfo.name = data.results[0].name.first;
-        usrinfo.image = data.results[0].picture.large;
+        userinfo.image = data.results[0].picture.large;
         userinfo.username = data.results[0].login.username;
         document.getElementById("myimage").src = userinfo.image;
         document.getElementById("myname").innerHTML = userinfo.name;
