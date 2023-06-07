54539ea0e64065caae337761c9a90a0075c170a9
UppalapatiSri <93474927+UppalapatiSri@users.noreply.github.com>
2023-06-07 19:18:04 +0530

    error corrected

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 var userinfo = {name : "", image : "", username : ""};
 
 var changemypic = function(){
-    fetch("https://randomuser.me/api").then((respone)=>response.json()).then((data)=>{ 
+    fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>{ 
         userinfo.name = data.results[0].name.first;
         userinfo.image = data.results[0].picture.large;
         userinfo.username = data.results[0].login.username;
